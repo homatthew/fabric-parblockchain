2faf7070e689610a25400d9373a4b9f7c36b59c3
matthew ho <matthewho@umail.ucsb.edu>
2020-06-17 02:28:38 -0700

    new fabric client version. 1.4.9 is incompatible

--- benchmarks/caliper/run-benchmark.sh
+++ benchmarks/caliper/run-benchmark.sh
@@ -1,5 +1,5 @@
 npx caliper launch master \
-  --caliper-bind-sut fabric:1.4.7 \
+  --caliper-bind-sut fabric:1.4.8 \
   --caliper-benchconfig benchmarks/ucsb-smallbank/config.yaml \
   --caliper-networkconfig networks/fabric/v1/v1.4.7-ucsb/2org1peergoleveldb_raft/fabric-go-tls.yaml \
   --caliper-workspace caliper-benchmarks   
--- benchmarks/caliper/setup.sh
+++ benchmarks/caliper/setup.sh
@@ -1,4 +1,4 @@
-npx caliper bind --caliper-bind-sut fabric:1.4.7 --caliper-bind-cwd ./
+npx caliper bind --caliper-bind-sut fabric:1.4.8 --caliper-bind-cwd ./
 cd caliper-benchmarks/networks/fabric/config_raft/
 ./generate.sh
 cd ../../../..
